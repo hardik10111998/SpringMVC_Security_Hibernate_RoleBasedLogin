86e2693082e67cba4accce508dcdf171ddec0df5
HARDIK <HARDIK@HARDIK>
2020-05-04 18:02:15 +0530

    Create Table Statements  Are Added

---
+++ SpringMVC_Security_Hibernate/Tabels.sql
@@ -0,0 +1,9 @@
+
+create table Authorities (id integer not null auto_increment, authority varchar(255), primary key (id));
+-------------------------------------------------------------------------------------------
+create table user_authority (USER_ID integer not null, AUTHORITY_ID integer not null, primary key (USER_ID, AUTHORITY_ID));
+------------------------------------------------------------------------------------------
+create table user_authority (USER_ID integer not null, AUTHORITY_ID integer not null, primary key (USER_ID, AUTHORITY_ID));
+-------------------------------------------------------------------------------------------
+alter table user_authority add constraint FK58ss3f2epuu8jbcbc1i040jcc foreign key (AUTHORITY_ID) references Authorities (id);
+alter table user_authority add constraint FKmo00qf8wms2g1u9dxyibaj835 foreign key (USER_ID) references User (userId);
